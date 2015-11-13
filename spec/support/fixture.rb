34e301923a7896b1dd6cfd5e732f9866e3c089c4
Andrew Myers <afredmyers@gmail.com>
2015-11-13 11:07:15 -0500

    Refactor spec fixtures just a little
    We basically just want a minimal wrapper around File.read, File.open that
    allows you to sepcify files relative to a base path, and gives specific errors
    when it breaks.

---
+++ spec/support/fixture.rb
@@ -0,0 +1,27 @@
+class Fixture
+
+  def self.base_path
+    # Default path is ../../fixtures, relative to this file.
+    @base_path ||= File.expand_path('fixtures', File.dirname(File.dirname(__FILE__)))
+  end
+
+  def self.base_path=(base_path)
+    @base_path = base_path
+  end
+
+  def self.full_path(relative_path)
+    File.join(Fixture.base_path, relative_path)
+  end
+
+  def self.open(relative_path, opts={})
+    filename = full_path(relative_path)
+    raise NotFound, "Fixture file #{filename} does not exist" unless File.exists?(filename)
+    File.open(filename, 'r')
+  end
+
+  def self.read(relative_path)
+    open(relative_path).read
+  end
+
+  class NotFound < StandardError; end
+end
