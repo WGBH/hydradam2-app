3ccfe2d2d848e9b166eedaff509393fe246b15ab
Andrew Myers <afredmyers@gmail.com>
2016-01-21 16:03:40 -0500

    Add class/module ancestor checking
    If a module is included into the wrong class, obscure errors result.
    This helps check for dependencies up front for better error reporting.

---
+++ lib/depends_on.rb
@@ -0,0 +1,21 @@
+require 'active_support'
+
+module DependsOn
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def depends_on(*args)
+      dependencies = args.map!(&:to_s)
+      ancestors = self.ancestors.to_a.map(&:name)
+      missing = dependencies - ancestors
+      raise MissingDependencies.new(self, missing) if missing.length > 0
+    end
+  end
+
+  class MissingDependencies < StandardError
+    def initialize(obj, missing)
+      msg = "class #{obj.name} must inherit from the following classes and/or modules: #{missing.join(', ')}"
+      super(msg)
+    end
+  end
+end
