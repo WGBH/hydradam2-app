776c84e062bc42ea99f182b525be6767bf72960d
amolmkhedkar <akhedkar@iu.edu>
2016-02-02 13:12:28 -0500

    Update has_ffprobe.rb

--- lib/hydradam/file_set_behavior/has_ffprobe.rb
+++ lib/hydradam/file_set_behavior/has_ffprobe.rb
@@ -1,25 +1,3 @@
-require 'depends_on'
-
-  module HydraDAM
-    module FileSetBehavior
-      module Hasffprobe
-        extend ActiveSupport::Concern
- 
-       included do
- 
-         # Ensure class dependencies
-         include DependsOn
-         depends_on(ActiveFedora::Base)
- 
-        # Ensure module dependencies
-        include Hydra::Works::FileSetBehavior
- 
-         # TODO: replace bogus predicate with legit one.
-        directly_contains_one :ffprobe, through: :files, type: ::RDF::URI('http://example.org/TODO-replace-with-actual-predicate'), class_name: 'XMLFile'
-      end
-    end
-  end
- end 
  require 'depends_on'
  
  module HydraDAM
