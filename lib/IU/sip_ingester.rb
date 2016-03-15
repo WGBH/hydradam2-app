b164cc3e0cea8acc5505452fc62993b78d92f025
Amol <akhedkar@iu.edu>
2016-03-15 15:09:58 -0400

    FFprobe file ingestion

--- lib/IU/sip_ingester.rb
+++ lib/IU/sip_ingester.rb
@@ -3,20 +3,53 @@ module IU
   class SIPIngester
 
     attr_accessor :path
+    attr_reader :ingested_objects, :depositor
     
-    def ffprobe
-      
-      File.new(ffprobe_path)
-      
+    def initialize(opts={})
 
+	@path = File.expand_path(:path) if opts.key?(:path)
+	@depositor = opts.delete(:depositor)
+	@ingested_objects = []
+	
+    end
 
+    def run!
+      
+      @ingested_objects << ffprobe_file_set
+       
     end
 
     def ffprobe_path
 
-        
-      Dir["#{path}/*ffprobe*"].first
+      filenames.select { |f| f =~ /.*ffprobe.*/ }.first
 
+    end
+
+    def filenames
+      
+       require 'pry'
+       binding.pry
+	
+      @filenames = begin
+        files = Dir["#{path}/*"]
+       #files.reject! {|filename| filename =~ /^\.\.?$/}
+        files.map! {|filename| File.expand_path(filename)}
+      end
+    end
+
+    def ffprobe_file_set
+
+      @ffprobe_file_set ||= begin
+        file_set = FileSet.new()
+        file_set.apply_depositor_metadata depositor
+        file_set.save!
+        file = File.open(ffprobe_path)
+        Hydra::Works::AddFileToFilSet.call(file_set, file, :ffprobe)
+        file.close
+        file_set.assign_properties_from_ffprobe
+        file_set.save!
+      end
+	
     end
 
   end
--- spec/lib/IU/sip_ingester_spec.rb
+++ spec/lib/IU/sip_ingester_spec.rb
@@ -1,21 +1,28 @@
 require "rails_helper"
 require "IU/sip_ingester"
 
-
 describe IU::SIPIngester do 
 
   before { subject.path = "spec/fixtures/IU_samples" }
 
   describe "#ffprobe" do
     
-    it "Returns a file object containign ffprobe XML" do
+       it "Returns a file object containing ffprobe XML" do
 
-      expect(subject.ffprobe).to be_a File
-  
- 
-   end
-	
+       expect(subject.ffprobe).to be_a File
+   
+       end	
   end  
+
+  describe '#ingested_objects' do
+    context 'before calling #run!' do
+      it 'returns an empty array' do
+        ingester = IU::FfprobeBatchIngester.new
+        expect(ingester.ingested_objects).to be_empty
+    end
+  end
+
+
 
   describe "#ffprobe_path" do
 
@@ -28,6 +35,20 @@ describe IU::SIPIngester do
     end
 
   end
+
+  def each_is_a?(objects, class_or_module)
+	objects.map{|obj| obj.is_a? class_or_module }.all?
+  end
+
+  def each_has_been_persisted?(objects)
+	objects.map {|obj| obj.persisted? }.all?    
+  end
+
+  it 'returns the same number of objects as files in the batch' do
+    expect(@ingester.ingested_objects,count)
+
+  end
+  
 
 end
 
