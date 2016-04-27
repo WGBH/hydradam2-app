8d6d7f8f0ec40474e6e8b970d3c2dd1f1b2c8f55
Andrew Myers <afredmyers@gmail.com>
2016-04-27 12:06:01 -0400

    Removes institution-sepcific data models
    Going forward, institution-specific data models will be generated
    in respetive branches. This is an intermediate step before turning
    HydraDAM in into a gem, and having institution's create their
    own CurationConcerns app, that contains all their extra customizations.

--- spec/lib/IU/models/file_set_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-require 'IU/models/file_set'
-
-describe IU::Models::FileSet do
-  it 'includes IU::Models::Concerns::FileSetBehavior' do
-    expect(described_class.ancestors).to include IU::Models::Concerns::FileSetBehavior
-  end
-end
