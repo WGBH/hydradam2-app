c70694d8c4d4099160f04e7ed521b3e4c579257d
Andrew Myers <afredmyers@gmail.com>
2016-04-28 10:18:00 -0400

    Adds file_format as a facet and searchable field

--- app/indexers/file_set_indexer.rb
+++ app/indexers/file_set_indexer.rb
@@ -2,6 +2,14 @@ class FileSetIndexer < CurationConcerns::FileSetIndexer
   def generate_solr_document
     super.tap do |solr_doc|
       solr_doc[Solrizer.solr_name('filename')] = object.filename
+
+      searchable_file_format = Solrizer.solr_name('file_format', :stored_searchable)
+      solr_doc[searchable_file_format] ||= []
+      solr_doc[searchable_file_format] += object.file_format
+
+      facetable_file_format = Solrizer.solr_name('file_format', :facetable)
+      solr_doc[facetable_file_format] ||= []
+      solr_doc[facetable_file_format] += object.file_format
     end
   end
 end
