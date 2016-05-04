ace3a77076de709aece38cd43089ced346e8a591
dlpierce <dlpierce@indiana.edu>
2016-05-04 16:38:29 -0400

    Add file_size_ltsi solr field plus a megabyte sized version.

--- app/indexers/file_set_indexer.rb
+++ app/indexers/file_set_indexer.rb
@@ -1,7 +1,12 @@
 class FileSetIndexer < CurationConcerns::FileSetIndexer
+
   def generate_solr_document
     super.tap do |solr_doc|
       solr_doc[Solrizer.solr_name('filename')] = object.filename
+
+      solr_doc.delete Solrizer.solr_name(:file_size, STORED_INTEGER)
+      solr_doc[Solrizer.solr_name(:file_size, Solrizer::Descriptor.new(:long, :stored, :indexed))] = object.file_size[0].to_i
+      solr_doc[Solrizer.solr_name(:file_size_mb, Solrizer::Descriptor.new(:long, :stored, :indexed))] = object.file_size[0].to_i / 1000000
 
       searchable_file_format = Solrizer.solr_name('file_format', :stored_searchable)
       solr_doc[searchable_file_format] ||= []
--- lib/hydradam/file_set_behavior/has_ffprobe.rb
+++ lib/hydradam/file_set_behavior/has_ffprobe.rb
@@ -10,10 +10,17 @@ module HydraDAM
         include DependsOn
         depends_on(ActiveFedora::Base)
 
-        property :file_format, predicate: RDF::Vocab::EBUCore.hasFileFormat
+        property :file_format, predicate: RDF::Vocab::EBUCore.hasFileFormat do |index|
+          index.as :symbol
+        end
 
-        # Include module dependencies
-        include ::CurationConcerns::FileSetBehavior
+        # property :fileSize, predicate: RDF::Vocab::EBUCore.fileSize, multiple: false do |index|
+        #   # index.as Solrizer::Descriptor.new(:long, :stored, :searchable)
+        # end
+
+ 
+        # Ensure module dependencies
+        include Hydra::Works::FileSetBehavior
  
          # TODO: replace bogus predicate with legit one.
         directly_contains_one :ffprobe, through: :files, type: ::RDF::URI('http://example.org/TODO-replace-with-actual-predicate'), class_name: 'XMLFile'
@@ -24,6 +31,7 @@ module HydraDAM
          noko.remove_namespaces!
          self.filename = noko.xpath('//ffprobe/format/@filename').text
          self.file_format += [noko.xpath('//ffprobe/format/@format_long_name').text]
+         self.file_size += [noko.xpath('//ffprobe/format/@size').text.to_i]
        end
      end
   end
