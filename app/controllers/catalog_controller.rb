11570a53d91086e6719fcacf408f85f879236e92
amolmkhedkar <akhedkar@iu.edu>
2016-05-10 14:35:05 -0400

    Initials for date range

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -58,6 +58,8 @@ class CatalogController < ApplicationController
 
     config.add_facet_field 'generic_type_sim', show: false, single: true
 
+    config.add_facet_field 'date_range', label: 'Date range', limit: 5, range: true
+
     # Have BL send all facet field names to Solr, which has been the default
     # previously. Simply remove these lines if you'd rather use Solr request
     # handler defaults, or have no facets.
