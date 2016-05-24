2966b9e27f22fc1c823975e08dd9a95ec69ece27
Randall Floyd <rdfloyd@indiana.edu>
2016-05-24 14:20:35 -0400

    HDM-395 Adding storage proxy client interactions to file_set views

--- app/controllers/storage_controller_behavior.rb
+++ app/controllers/storage_controller_behavior.rb
@@ -1,12 +1,20 @@
 module StorageControllerBehavior
+  require 'hydradam/storage_proxy_client'
 
   def show
     @file_set = curation_concern_type.load_instance_from_solr(params[:id]) unless curation_concern
     @filename = File.basename(@file_set.filename)
     @file_status_resp = get_file_status
+    if params[:next_action] == 'file_status'
+      @file_status_resp = get_file_status
+    elsif params[:next_action] == 'stage'
+      @file_status_resp = stage_file
+    elsif params[:next_action] == 'unstage'
+      @file_status_resp = unstage_file
+    end
+    puts params[:next_action]
     puts @file_status_resp
-    #puts @file_set.filename
-    #puts params[:next_action]
+
     respond_to do |wants|
       wants.html { presenter }
     end
@@ -35,19 +43,20 @@ module StorageControllerBehavior
 
   def stage_file
     get_proxy_client
-    @sp_client.filename = filename
+    @sp_client.filename = @filename
     response = @sp_client.stage
     response.body
   end
 
   def unstage_file
     get_proxy_client
-    @sp_client.filename = filename
-    response = @sp_client.stage
+    @sp_client.filename = @filename
+    response = @sp_client.unstage
     response.body
   end
 
   def get_proxy_client
+    @sp_client = HydraDAM::StorageProxyClient.new
     begin
       @sp_client = HydraDAM::StorageProxyClient.new
     rescue
--- spec/controllers/storage_controller_behavior_spec.rb
+++ spec/controllers/storage_controller_behavior_spec.rb
@@ -39,16 +39,41 @@ describe  CurationConcerns::FileSetsController, type: :controller do
 
   end
 
-  let!(:file_set) { FileSet.new(id: '1', filename: 'unstaged_file')}
-
   describe 'when signed in' do
     before { sign_in depositor }
     before { sip.ingest! }
 
+    describe '#show' do
+
+      it "redirects_to :action => :show" do
+        #get :show, id: sip.access_copy.id, next_action: 'stage'
+        #expect(subject).to redirect_to :action => :show, :id => sip.access_copy.id
+      end
+      context 'params[:next_action] eq file_status' do
+        it 'stores a JSON response from the storage proxy' do
+          get :show, id: sip.access_copy.id, next_action: 'file_status'
+          expect(JSON.parse(assigns(:file_status_resp))["name"]).to eq File.basename(sip.access_copy.filename)
+        end
+      end
+      context 'params[:next_action] eq stage' do
+        it 'stores a JSON response from the storage proxy' do
+          get :show, id: sip.access_copy.id, next_action: 'stage'
+          expect(JSON.parse(assigns(:file_status_resp))["type"]).to eq 'stage'
+        end
+      end
+      context 'params[:next_action] eq unstage' do
+        it 'stores a JSON response from the storage proxy' do
+          get :show, id: sip.access_copy.id, next_action: 'unstage'
+          expect(JSON.parse(assigns(:file_status_resp))["type"]).to eq 'unstage'
+        end
+      end
+
+    end
+
     describe '#file_status' do
 
       it "redirects_to :action => :show" do
-        get :show, id: sip.access_copy.id
+        #get :show, id: sip.access_copy.id, next_action: 'file_status'
         #expect(subject).to redirect_to :action => :show, :id => sip.access_copy.id
       end
       it 'stores a JSON response from the storage proxy' do
