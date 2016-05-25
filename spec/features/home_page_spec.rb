0738bbf0d4eb9afcc5b64fa226f4f6cfcfbdaa3b
amolmkhedkar <akhedkar@iu.edu>
2016-05-25 13:54:38 -0400

    Add specs for home page blurb.

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe 'Sanity check for the root URL' do
   before { visit '/' }
   it 'has home page content' do
-    expect(body).to have_text 'HydraDAM is a digital preservation repository'
+    expect(body).to have_text 'HydraDAM2 is an audiovisual digital preservation repository'
   end
 end
