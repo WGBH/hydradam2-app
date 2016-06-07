0b9ba335613196297b5638e85b063cbef3658134
amolmkhedkar <akhedkar@iu.edu>
2016-06-07 15:30:04 -0400

    Add dtsi fields to work_presenter to be delegated to solr_document

--- app/presenters/curation_concerns/work_presenter.rb
+++ app/presenters/curation_concerns/work_presenter.rb
@@ -1,10 +1,10 @@
 module CurationConcerns
   class WorkPresenter < CurationConcerns::WorkShowPresenter
+
     delegate :digitized_by_entity, :digitized_by_staff, :mdpi_timestamp, :extraction_workstation, :digitization_comments,
              :original_identifier, :definition, :mdpi_barcode, :unit_of_origin, :original_format, :recording_standard,
-             :image_format,
+             :image_format, :system_create_dtsi, :system_modified_dtsi,
              to: :solr_document
-
 
   end
 end
