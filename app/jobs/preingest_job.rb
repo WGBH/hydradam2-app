c2019df63279da8c1038e5ea7829b470354f205f
Adam Ploshay <aploshay@iu.edu>
2016-11-21 15:34:59 -0500

    Stub preingest task: finds preingest file, outputs yaml, but with stub values

--- app/jobs/preingest_job.rb
+++ app/jobs/preingest_job.rb
@@ -1,9 +1,9 @@
 class PreingestJob < ActiveJob::Base
   queue_as :preingest
 
-  def perform(document_class, preingest_file, user)
-    logger.info "Preingesting #{document_class} #{preingest_file}"
-    @document = document_class.new preingest_file
+  def perform(preingest_file, user)
+    logger.info "Preingesting #{preingest_file}"
+    @preingest_file = preingest_file
     @user = user
 
     preingest
@@ -13,20 +13,14 @@ class PreingestJob < ActiveJob::Base
 
     def preingest
       yaml_hash = {}
-      yaml_hash[:resource] = @document.resource_class.to_s
-      yaml_hash[:attributes] = @document.attributes
-      yaml_hash[:source_metadata] = @document.source_metadata
-      
-      if @document.multi_volume?
-        yaml_hash[:volumes] = @document.volumes
-      else
-        yaml_hash[:structure] = @document.structure
-        yaml_hash[:files] = @document.files
-      end
+      yaml_hash[:resource] = Work.to_s
+      yaml_hash[:attributes] = {}
+      yaml_hash[:source_metadata] = nil
+      yaml_hash[:files] = []
+      yaml_hash[:sources] = []
 
-      yaml_hash[:sources] = [{ title: @document.source_title, file: @document.source_file }]
-
-      File.write(@document.yaml_file, yaml_hash.to_yaml)
-      logger.info "Created YAML file #{File.basename(@document.yaml_file)}"
+      output_file = @preingest_file.gsub(/\..{3,4}/, '.yml')
+      File.write(output_file, yaml_hash.to_yaml)
+      logger.info "Created YAML file #{output_file}"
     end
 end
