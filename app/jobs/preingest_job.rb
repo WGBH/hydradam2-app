65dd9e116c9f4f4691a94e5af1d73e9720cffd4a
Adam Ploshay <aploshay@iu.edu>
2016-11-21 15:53:08 -0500

    Preingest now unpacks files, adds filename list to output, deletes unpacked files

--- app/jobs/preingest_job.rb
+++ app/jobs/preingest_job.rb
@@ -12,15 +12,52 @@ class PreingestJob < ActiveJob::Base
   private
 
     def preingest
-      yaml_hash = {}
-      yaml_hash[:resource] = Work.to_s
-      yaml_hash[:attributes] = {}
-      yaml_hash[:source_metadata] = nil
-      yaml_hash[:files] = []
-      yaml_hash[:sources] = []
+      @yaml_hash = {}
+      @yaml_hash[:resource] = Work.to_s
+      @yaml_hash[:attributes] = {}
+      @yaml_hash[:source_metadata] = nil
+      @yaml_hash[:files] = []
+      @yaml_hash[:sources] = []
+
+      filenames.each { |filename| process_file(filename) }
+      delete_extracted_files!
 
       output_file = @preingest_file.gsub(/\..{3,4}/, '.yml')
-      File.write(output_file, yaml_hash.to_yaml)
+      File.write(output_file, @yaml_hash.to_yaml)
       logger.info "Created YAML file #{output_file}"
+    end
+
+    def tarball_entries
+      @tarball_entries ||= begin
+        File.open(@preingest_file, 'rb') do |file|
+          Archive::Tar::Minitar.open(file).map{ |entry| entry }.to_a
+        end
+      end
+    end
+
+    def filenames
+      @filenames ||= Dir["#{root_dir}/*"]
+    end
+
+    def root_dir
+      @root_dir ||= begin
+        root_dir_parent = File.dirname(@preingest_file)
+        Archive::Tar::Minitar.unpack(@preingest_file, root_dir_parent)
+        root_dir_basename = tarball_entries.select{ |tar_entry| tar_entry.directory? }.first.name
+        File.expand_path(root_dir_basename, root_dir_parent)
+      end
+
+      raise ExtractionError unless File.directory?(@root_dir)
+      @root_dir
+    end
+
+    def delete_extracted_files!
+      FileUtils.remove_entry_secure(root_dir)
+    end
+
+    def process_file(filename)
+      file_hash = {}
+      file_hash[:filename] = filename
+      @yaml_hash[:files] << file_hash 
     end
 end
