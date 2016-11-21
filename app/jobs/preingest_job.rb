979355e81a8660fc894e7760969c1b1b30a6f03a
amolmkhedkar <akhedkar@iu.edu>
2016-11-21 10:04:58 -0500

    Update preingest_job.rb

--- app/jobs/preingest_job.rb
+++ app/jobs/preingest_job.rb
@@ -1,9 +1,6 @@
 class PreingestJob < ActiveJob::Base
   queue_as :preingest
 
-  # @param [Class] document_class Class of preingestable document wrapper
-  # @param [String] preingest_file Filename of a preingest file to preingest
-  # @param [String] user User to ingest as
   def perform(document_class, preingest_file, user)
     logger.info "Preingesting #{document_class} #{preingest_file}"
     @document = document_class.new preingest_file
@@ -19,9 +16,7 @@ class PreingestJob < ActiveJob::Base
       yaml_hash[:resource] = @document.resource_class.to_s
       yaml_hash[:attributes] = @document.attributes
       yaml_hash[:source_metadata] = @document.source_metadata
-      yaml_hash[:thumbnail_path] = @document.thumbnail_path
-      yaml_hash[:collections] = @document.collections
-
+      
       if @document.multi_volume?
         yaml_hash[:volumes] = @document.volumes
       else
