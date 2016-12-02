b1a38244f5730c3eada6090617d1ec03e823f8c5
amolmkhedkar <akhedkar@iu.edu>
2016-12-02 11:15:13 -0500

    Added comments about the predicate is same or different as context.yml for this property

--- app/models/concerns/work_behavior.rb
+++ app/models/concerns/work_behavior.rb
@@ -10,24 +10,38 @@ module Concerns
       contains :pod_xml, class_name: "XMLFile"
 
       # From mdpi_xml
-      property :digitized_by_entity, predicate: RDF::Vocab::EBUCore.hasCreator do |index|
+      
+      # Predicate is same as context.yml for this property
+      property :digitized_by_entity, predicate: RDF::Vocab::EBUCore.hasCreator do |index| 
         index.as :stored_searchable, :facetable
       end
+      
+      # Predicate is same as context.yml for this property
       property :digitized_by_staff, predicate: RDF::Vocab::EBUCore.hasMetadataAttributor do |index|
         index.as :stored_searchable, :facetable
       end
+      
+      # I do not know if this is needed as of now as the property is eracsed from the Google doc     
       property :mdpi_date, predicate: RDF::Vocab::EBUCore.dateDigitised, multiple: false do |index|
         index.as :stored_searchable, :sortable, :facetable
       end
+      
+      # Predicate is not same as context.yml for this property
       property :extraction_workstation, predicate: RDF::Vocab::EBUCore.textualAnnotation do |index|
         index.as :stored_searchable, :facetable
       end
+      
+      # Predicate is same as context.yml for this property
       property :digitization_comments, predicate: RDF::Vocab::EBUCore.comments do |index|
         index.as :stored_searchable
       end
+      
+       # Predicate is same as context.yml for this property
       property :original_identifier, predicate: RDF::Vocab::EBUCore.hasSource do |index|
         index.as :symbol
       end
+      
+      # Predicate is same as context.yml for this property
       property :definition, predicate: RDF::Vocab::EBUCore.hasVideoFormat do |index|
         index.as :stored_searchable, :facetable
       end
@@ -38,18 +52,28 @@ module Concerns
       # :title property included in core behaviors
 
       # From pod_xml
+      
+      # The attribute name is identifier in Google doc and the Predicate is same as context.yml for this property
       property :mdpi_barcode, predicate: RDF::Vocab::EBUCore.identifier, multiple: false do |index|
         index.as :symbol
       end
+      
+       # Predicate is not same as context.yml for this property      
       property :unit_of_origin, predicate: RDF::Vocab::EBUCore.isOwnedBy do |index|
         index.as :stored_searchable, :facetable
       end
+      
+       # Predicate is not same as context.yml for this property    
       property :original_format, predicate: RDF::Vocab::EBUCore.hasFormat do |index|
         index.as :stored_searchable, :facetable
       end
+      
+      # Predicate is same as context.yml for this property
       property :recording_standard, predicate: RDF::Vocab::EBUCore.hasStandard do |index|
         index.as :stored_searchable, :facetable
       end
+      
+      # Predicate is same as context.yml for this property
       property :image_format, predicate: RDF::Vocab::EBUCore.aspectRatio do |index|
         index.as :stored_searchable, :facetable
       end
