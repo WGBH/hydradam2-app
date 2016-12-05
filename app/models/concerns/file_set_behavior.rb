f07919ecb5fd4633a49f0ce0ad11f1b544784ca5
amolmkhedkar <akhedkar@iu.edu>
2016-12-05 14:33:13 -0500

    Added file_format, file_format_long_name and codec_type.

--- app/models/concerns/file_set_behavior.rb
+++ app/models/concerns/file_set_behavior.rb
@@ -12,7 +12,19 @@ module Concerns
     include HydraDAM::FileSetBehavior::HasFfprobe
     
    property :date_generated: RDF::Vocab::EBUCore:dateCreated do |index|
-      index.as :stored_searchable
+      index.as :stored_searchable, :facetable, :stored_sortable
    end
+   
+   property :file_format: RDF::Vocab::PREMIS:hasFormatName do |index|
+      index.as :stored_sortable, :facetable
+   end
+    
+   property :file_format_long_name: RDF::Vocab::EBUCore:hasFileFormat do |index|
+      index.as :stored_sortable, :facetable
+   end
+    
+   property :codec_type: RDF::Vocab::EBUCore:hasMedium do |index|
+      index.as :stored_sortable, :facetable
+   end    
   end
 end
