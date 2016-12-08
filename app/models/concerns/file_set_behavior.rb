73d76d6e94c149723a9b3a112356779cd40fe0e0
Adam Ploshay <aploshay@iu.edu>
2016-12-07 19:08:12 -0500

    Fixed broken FileSetBehavior

--- app/models/concerns/file_set_behavior.rb
+++ app/models/concerns/file_set_behavior.rb
@@ -10,63 +10,66 @@ module Concerns
     include HydraDAM::FileSetBehavior::HasMDPI
     include HydraDAM::FileSetBehavior::HasPod
     include HydraDAM::FileSetBehavior::HasFfprobe
-    
-   property :date_generated: RDF::Vocab::EBUCore:dateCreated do |index|
-      index.as :stored_searchable, :facetable, :stored_sortable
-   end
-   
-   property :file_format: RDF::Vocab::PREMIS:hasFormatName do |index|
-      index.as :stored_sortable, :facetable
-   end
-    
-   property :file_format_long_name: RDF::Vocab::EBUCore:hasFileFormat do |index|
-      index.as :stored_sortable, :facetable
-   end
-    
-   property :codec_type: RDF::Vocab::EBUCore:hasMedium do |index|
-      index.as :stored_sortable, :facetable
-   end
-    
-   property :codec_name: RDF::Vocab::EBUCore:hasCode do |index|
-      index.as :stored_sortable, :facetable
-   end
-    
-   property :codec_long_name: RDF::Vocab::EBUCore:codecName 
-   #do |index|
-   #   index.as :stored_sortable, :facetable
-   #end
-    
-   property :duration: RDF::Vocab::EBUCore:duration do |index|
-      index.as :stored_sortable, :facetable
-   end 
-    
-   property :bit_rate: RDF::Vocab::EBUCore:bitRate 
-   
-   property :file_name: RDF::Vocab::EBUCore:filename do |index|
-      index.as :stored_searchable
-   end 
-    
-   property :file_size: RDF::Vocab::EBUCore:fileSize 
-    
-   property :identifier: RDF::Vocab::EBUCore:identifier do |index|
-      index.as :stored_searchable
-   end 
-    
-   property :unit_of_origin: RDF::Vocab::EBUCore:comments do |index|
-      index.as :stored_searchable, :facetable, :stored_sortable
-   end 
-   
-   property :part: RDF::Vocab::EBUCore:partNumber
-    
-   property :sample_rate: RDF::Vocab::EBUCore:sampleRate 
-    
-   property :video_width: RDF::Vocab::EBUCore:width 
-    
-   property :video_height: RDF::Vocab::EBUCore:height
-    
-   property :md5_checksum: RDF::Vocab::NFO:hashValue do |index|
-      index.as :stored_searchable
-   end 
-    
+
+    included do    
+
+     property :date_generated, predicate: RDF::Vocab::EBUCore.dateCreated do |index|
+        index.as :stored_searchable, :facetable, :stored_sortable
+     end
+     
+     property :file_format, predicate: RDF::Vocab::PREMIS.hasFormatName do |index|
+        index.as :stored_sortable, :facetable
+     end
+      
+     property :file_format_long_name, predicate: RDF::Vocab::EBUCore.hasFileFormat do |index|
+        index.as :stored_sortable, :facetable
+     end
+      
+     property :codec_type, predicate: RDF::Vocab::EBUCore.hasMedium do |index|
+        index.as :stored_sortable, :facetable
+     end
+      
+     property :codec_name, predicate: RDF::Vocab::EBUCore.hasCodec do |index|
+        index.as :stored_sortable, :facetable
+     end
+      
+     property :codec_long_name, predicate: RDF::Vocab::EBUCore.codecName 
+     #do |index|
+     #   index.as :stored_sortable, :facetable
+     #end
+      
+     property :duration, predicate: RDF::Vocab::EBUCore.duration do |index|
+        index.as :stored_sortable, :facetable
+     end 
+      
+     property :bit_rate, predicate: RDF::Vocab::EBUCore.bitRate 
+     
+     property :file_name, predicate: RDF::Vocab::EBUCore.filename do |index|
+        index.as :stored_searchable
+     end 
+      
+     property :file_size, predicate: RDF::Vocab::EBUCore.fileSize 
+      
+     property :identifier, predicate: RDF::Vocab::EBUCore.identifier do |index|
+        index.as :stored_searchable
+     end 
+      
+     property :unit_of_origin, predicate: RDF::Vocab::EBUCore.comments do |index|
+        index.as :stored_searchable, :facetable, :stored_sortable
+     end 
+     
+     property :part, predicate: RDF::Vocab::EBUCore.partNumber
+      
+     property :sample_rate, predicate: RDF::Vocab::EBUCore.sampleRate 
+      
+     property :video_width, predicate: RDF::Vocab::EBUCore.width 
+      
+     property :video_height, predicate: RDF::Vocab::EBUCore.height
+      
+     property :md5_checksum, predicate: RDF::Vocab::NFO.hashValue do |index|
+        index.as :stored_searchable
+     end 
+
+    end
   end
 end
