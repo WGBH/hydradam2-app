8bc10c376b06952258dcce3f00b5e1d554e416bb
Adam Ploshay <aploshay@iu.edu>
2016-12-22 12:47:02 -0500

    [HPT-728] Basic Yaml preingest (sanitization, only)

--- lib/hydradam/preingest/IU/yaml.rb
+++ lib/hydradam/preingest/IU/yaml.rb
@@ -7,6 +7,7 @@ module HydraDAM
       class Yaml
         def initialize(preingest_file)
           @preingest_file = preingest_file
+          @yaml = File.open(preingest_file) { |f| Psych.load(f) }
           parse
         end
         attr_reader :preingest_file
@@ -21,11 +22,20 @@ module HydraDAM
         end
 
         def parse
-          @work_attributes = {}
-          @file_set_attributes = {}
-          @source_metadata = nil
-          @file_sets = []
-          @sources = []
+          @work_attributes = @yaml[:work_attributes]
+          @work_attributes.each do |source, attributes_set|
+            @work_attributes[source] = HydraDAM::Preingest::AttributeIngester.new(@preingest_file, attributes_set, factory: resource_class).raw_attributes
+          end
+          @file_set_attributes = @yaml[:file_set_attributes]
+          @file_set_attributes.each do |source, attributes_set|
+            @file_set_attributes[source] = HydraDAM::Preingest::AttributeIngester.new(@preingest_file, attributes_set, factory: FileSet).raw_attributes
+          end
+          @source_metadata = @yaml[:source_metadata]
+          @file_sets = @yaml[:file_sets]
+          @file_sets.select { |fs| fs[:attributes].present? }.each do |file_set|
+            file_set[:attributes] = HydraDAM::Preingest::AttributeIngester.new(@preingest_file, file_set[:attributes], factory: FileSet).raw_attributes
+          end
+          @sources = @yaml[:sources]
         end
       end
     end
