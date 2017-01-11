141e780ce5a1bd66793c905ad3861f31de75c435
amolmkhedkar <akhedkar@iu.edu>
2017-01-11 16:07:06 -0500

    Create has_mods.rb

---
+++ lib/hydradam/file_set_behavior/has_mods.rb
@@ -0,0 +1,30 @@
+require 'depends_on'
+ 
+module HydraDAM
+  module FileSetBehavior
+    module HasMods
+      extend ActiveSupport::Concern
+ 
+      included do 
+        # Ensure class dependencies
+        include DependsOn
+        depends_on(ActiveFedora::Base)
+
+        property :title, predicate: RDF::Vocab::EBUCore.title do |index|
+          index.as :stored_searchable, :sortable, :facetable
+        end
+
+        # Ensure module dependencies
+        include ::CurationConcerns::FileSetBehavior
+      end
+ 
+      def assign_properties_from_mods
+        noko = parent.mods_xml.noko.dup
+        noko.remove_namespaces!
+        self.title += [noko.xpath('/mods/titleInfo/title').text]
+      end
+
+
+    end
+  end
+end
