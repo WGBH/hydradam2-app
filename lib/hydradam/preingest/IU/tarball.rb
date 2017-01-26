fcb5500df23ad15b392ba750c9d55f85c42fcc10
amolmkhedkar <akhedkar@iu.edu>
2017-01-26 10:56:34 -0500

    Add premis_event_detail as FFprobe multimedia streams analyzer from FFmpeg

--- lib/hydradam/preingest/IU/tarball.rb
+++ lib/hydradam/preingest/IU/tarball.rb
@@ -334,18 +334,20 @@ module HydraDAM
           # FIXME: Minitar's unpack does not allow --atime-preserve argument, to maintain timestamps
           attributes[:premis_event_date_time] = Array.wrap(File.mtime(id))
           # FIXME: the preservation gem doesn't have these attributes yet
-          # attributes[:premis_event_detail] = ['foo']
+          attributes[:premis_event_detail] = ['FFprobe multimedia streams analyzer from FFmpeg']
           # attributes[:premis_event_outcome] = ['PASS']
           results << { attributes: attributes }
           attributes = {}
           attributes[:premis_event_type] = ['cre']
           attributes[:premis_agent] = ['mailto:' + User.first&.email]
           attributes[:premis_event_date_time] = Array.wrap(File.mtime(id))
+	  attributes[:premis_event_detail] = ['FFprobe multimedia streams analyzer from FFmpeg']
           results << { attributes: attributes }
 	  attributes = {}
           attributes[:premis_event_type] = ['mes']
           attributes[:premis_agent] = ['mailto:' + User.first&.email]
           attributes[:premis_event_date_time] = Array.wrap(File.mtime(id))
+	  attributes[:premis_event_detail] = ['FFprobe multimedia streams analyzer from FFmpeg']
           results << { attributes: attributes }
           results
         end
