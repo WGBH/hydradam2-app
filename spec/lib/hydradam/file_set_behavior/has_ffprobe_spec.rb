f081fbe492aab07477ae6718ad2ed0df09b44533
amolmkhedkar <akhedkar@iu.edu>
2017-02-15 15:09:14 -0500

    Skips tests in has_ffprobe_spec.rb

--- spec/lib/hydradam/file_set_behavior/has_ffprobe_spec.rb
+++ spec/lib/hydradam/file_set_behavior/has_ffprobe_spec.rb
@@ -28,12 +28,36 @@ describe HydraDAM::FileSetBehavior::HasFfprobe, :requires_fedora do
     subject.delete rescue nil
   end
 
- # it 'exposes accessors #ffprobe and #ffprobe=' do
- #   expect(subject).to respond_to :ffprobe
- #   expect(subject).to respond_to :"ffprobe="
- # end
+ xit 'exposes accessors #ffprobe and #ffprobe=' do
+    expect(subject).to respond_to :ffprobe
+    expect(subject).to respond_to :"ffprobe="
+ end
 
- context 'when the including class does not inherit from ActiveFedora::Base' do
+  describe '#ffprobe=' do
+    xit 'requires an XMLFile' do
+      expect{ subject.ffprobe = "this will fail" }.to raise_error ActiveFedora::AssociationTypeMismatch
+    end
+
+    xit 'accepts a XMLFile' do
+      subject.save! # the parent object must be saved before attaching files.
+      expect{ subject.ffprobe = XMLFile.new }.to_not raise_error
+    end
+  end
+
+  describe '#assign_properties_from_ffprobe' do
+    let(:ffprobe_file) { File.open('./spec/fixtures/ffprobe/ffprobe_1.xml') }
+
+    before do
+      Hydra::Works::AddFileToFileSet.call(subject, ffprobe_file, :ffprobe)
+      subject.assign_properties_from_ffprobe
+    end
+
+    xit 'assigns values from ffprobe XML file to RDF properties on the object' do
+      expect(subject.filename).to eq "/cookies/pizza.wav"
+    end
+  end 
+  
+  context 'when the including class does not inherit from ActiveFedora::Base' do
     let(:class_with_missing_dependency) do
       # An anonymous class that includes the HydraDAM::FileSetBehavior::HasFfprobe
       # module but does not inherity from ActiveFedora::Base like it should
